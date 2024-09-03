06647e3c67167d78764e8a382d82f63b746f8631
Tracy Huang <chiayu561@g.ucla.edu>
2024-09-03 20:58:27 +0800

    refactor: combine rrc.m and rrc2.m into rrm.m to improve code clarity

--- src/rrc.m
+++ src/rrc.m
@@ -7,13 +7,20 @@ function h = rrc(t,alpha,T)
 %       - alpha: Roll-off factor of RRC filter (0<=alpha<=1)
 %       - T: Symbol period (T=1/Rs)
 % OUTPUT: 
-%       - P: Impulse response of RCC filter, denoted as h(t) in time domain
+%       - h: Impulse response of RCC filter, denoted as h(t) in time domain
 
-% t(t==0) = eps; % T/1e5; % gets rid of t==0, which is not defined
+% To see the detailed formula for impulse response h(t) of RRC filter,
+% please refer to the corresponding formula in the text section of the
+% MATLAB live script.
 h = zeros(size(t));
-h(t~=0) = 4*alpha/(pi*sqrt(T)) * ( cos((1+alpha)*pi*t(t~=0)/T) +...
-    sin((1-alpha)*pi*t(t~=0)/T)./(4*alpha*t(t~=0)/T) ) ./...
-    (1 - (4*alpha*t(t~=0)/T).^2);
-h(t==0) = (pi+alpha*(4-pi))/(pi*sqrt(T));
-h((t==T/4/alpha)|(t==-T/4/alpha)) = (alpha/sqrt(2*T))*...
-    ((1+2/pi)*sin(pi/4/alpha) + (1-2/pi)*cos(pi/4/alpha));
+for i=1:length(t)
+    if t(i)==0
+        h(i)=(1+alpha*(4/pi-1))/T;
+    elseif t(i)==T/(4*alpha) || t(i)==-T/(4*alpha)
+        h(i)=(alpha/T/sqrt(2))*((1+2/pi)*sin(pi/4/alpha)+...
+              (1-2/pi)*cos(pi/4/alpha));
+    else
+        h(i)=(sin((pi*t(i)/T)*(1-alpha))+(4*alpha*t(i)/T)*cos((pi*t(i)/T)*(1+alpha)))/...
+             ((pi*t(i))*(1-(4*alpha*t(i)/T)^2));
+    end
+end
